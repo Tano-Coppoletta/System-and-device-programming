defcc499c59ed1c6b52579d1fa951cbeecb86c38
gaetano <s301131@studenti.polito.it>
2022-04-16 10:00:15 +0200

    ex1

--- Labs/part1/lab04/ex01.c
+++ Labs/part1/lab04/ex01.c
@@ -27,8 +27,8 @@ sem_t mutex; //mutex to access n_thread
 
 int main(int argc,char *argv[]){
     int n,n_elements;
-    //int *v;
-    int v[]={2,4,6,1,3,5,8,7};
+    int *v;
+    //int v[]={2,4,6,1,3,5,8,7};
     pthread_t *tid;
     thread_t *ts;
 
@@ -38,16 +38,16 @@ int main(int argc,char *argv[]){
     }
 
     n=atoi(argv[1]);
-    //printf("n=%d\n",n);
+    printf("n=%d\n",n);
     n_elements=(1 << n);
     n_thread=n_elements-1;
-    //printf("n_elements=%d\n",n_elements);
+    printf("n_elements=%d\n",n_elements);
 
     //allocation of v and tid  and ts
-    //v=malloc(n_elements*sizeof(int));
+    v=malloc(n_elements*sizeof(int));
     tid=malloc((n_elements-1)*sizeof(pthread_t));
     ts=malloc((n_elements-1)*sizeof(thread_t));
-    if(/*v==NULL ||*/ tid==NULL){
+    if(v==NULL || tid==NULL){
         fprintf(stderr,"Error in malloc v\n");
         exit(1);
     }
@@ -55,12 +55,17 @@ int main(int argc,char *argv[]){
     //initialize mutex
     sem_init(&mutex,0,1);
     //generate elements of the array
- /*   for(int i=0;i<n_elements;i++){
+    for(int i=0;i<n_elements;i++){
         v[i]= rand() % 9 + 1; //from 1 to 9
         fprintf(stdout,"%d ",v[i]);
     }
     fprintf(stdout,"\n");
-*/
+
+/*
+    for(int i=0;i<n_elements;i++){
+        fprintf(stdout,"%d ",v[i]);
+    }
+    fprintf(stdout,"\n");*/
     //init barrier
     pthread_barrier_init(&bar_read,NULL,n_elements-1);
     pthread_barrier_init(&bar_write,NULL,n_elements-1);
@@ -78,8 +83,14 @@ int main(int argc,char *argv[]){
     }
 
     //destroy barrier
-   // pthread_barrier_destroy(&bar_read);
-   // pthread_barrier_destroy(&bar_write);
+    pthread_barrier_destroy(&bar_read);
+    pthread_barrier_destroy(&bar_write);
+
+   
+    for(int i=0;i<n_elements;i++){
+        fprintf(stdout,"%d ",v[i]);
+    }
+    fprintf(stdout,"\n");
 
     //free
    // free(v);
@@ -89,7 +100,7 @@ int main(int argc,char *argv[]){
 
 void *tF(void* arg){
     thread_t *ts=(thread_t*)arg;
-    int gap;
+    int gap=1;
 
     for(int i=0;i<ts->n;i++){
         gap=1<<i;
@@ -99,41 +110,37 @@ void *tF(void* arg){
         
         fprintf(stdout,"T: %d sum: %d\n",ts->id,ts->sum);
         
-
-
-        printf("exit bar_read T:%d\n",ts->id);
-
-        if(ts->id<=i+1){
+        if(ts->id<(gap+1)){
             //reinitialize the barrier and exit
             sem_wait(&mutex);
+            //update n_thread
+             n_thread--;
             
+            pthread_barrier_destroy(&bar_write);
             
             pthread_barrier_init(&bar_write,NULL,n_thread);
 
             sem_post(&mutex);
               
         }
-
-         //wait on the barrier
+         //wait on the barrier of the reader
         pthread_barrier_wait(&bar_read);
-        
 
         //write the sum in the array
 
         ts->v[ts->id]=ts->sum;
 
-        printf("%d\n",n_thread);
-        
-
-        if(ts->id<=i+1){
+        if(ts->id<(gap+1)){
             //reinitialize the barrier and exit
             fprintf(stdout,"Killing t: %d\n",ts->id);
             sem_wait(&mutex);
-            n_thread--;
+           
             
             fprintf(stdout,"n_thread: %d\n",n_thread);
+
+            pthread_barrier_destroy(&bar_read);
+
             pthread_barrier_init(&bar_read,NULL,n_thread);
-           // pthread_barrier_init(&bar_read,NULL,n_thread);
 
             sem_post(&mutex);
             

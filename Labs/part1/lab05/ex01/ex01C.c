6a2f0d1139be05c1850733e8371ffc1467417ca2
gaetano <s301131@studenti.polito.it>
2022-05-02 22:08:42 +0200

    ex1

--- Labs/part1/lab05/ex01/ex01C.c
+++ Labs/part1/lab05/ex01/ex01C.c
@@ -10,31 +10,31 @@
 #include <fcntl.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/msg.h>
+
 
 #define SHM_SIZE 1024
 
 extern int errno;
 
-void P1(char *readFifo,char* writeFifo,int shmid);
-void P2(char *readFifo,char *writeFifo,int shmid);
+void P1(key_t keyq1,key_t keyq2,int shmid);
+void P2(key_t keyq1, key_t keyq2,int shmid);
 void upper_string(char *str,int length);
+
+
+typedef struct mymesg{
+    long int mtype;
+    int size;
+}message;
 
 int main(int argc, char* argv[]){
     setbuf(stdout, 0);
 
     key_t key;
-    int shmid;
-    char *fifo1="./tmp/fifo1";
-    char *fifo2="./tmp/fifo2";
+    key_t key_queue1,key_queue2;
+    int shmid,msqid,msqid2;
 
-    if(mkfifo(fifo1,0666)!=0){
-        perror("mkfifo1");
-        exit(1);
-    }
-    if(mkfifo(fifo2,0666)!=0){
-        perror("mkfifo2");
-        exit(1);
-    }
+
 
 
     //generate the key
@@ -48,32 +48,43 @@ int main(int argc, char* argv[]){
         exit(1);
     }
 
+    //generate the key for the msg queue
+    if((key_queue1=ftok("key.txt",5))==-1){
+        perror("ftok keyqueue");
+        exit(1);
+    }
+
+
+    //second msg queue
+    if((key_queue2=ftok("key.txt",6))==-1){
+        perror("ftok keyqueue");
+        exit(1);        
+    }
+
+
 
     if(fork()){
         //P1
 
-        P1(fifo1,fifo2,shmid);
+        P1(key_queue1,key_queue2,shmid);
        
 
     }else{
 
-        P2(fifo2,fifo1,shmid);
+        P2(key_queue2,key_queue1,shmid);
 
     }
-    //delete fifo files
-    char str[50];
-    sprintf(str,"rm -rf %s",fifo1);
-    system(str);
-    sprintf(str,"rm -rf %s",fifo2);
-    system(str);    
+  
 
 }
 
 
-void P1(char *readFifo,char *writeFifo,int shmid){
+void P1(key_t keyq1,key_t keyq2,int shmid){
     int i;
     char *string,*data;
     unsigned int seed=1;
+    int msgid1,msgid2;
+    message msg1,msg2;
 
 
     //attach to the shared memory
@@ -83,24 +94,17 @@ void P1(char *readFifo,char *writeFifo,int shmid){
         exit(1);
     }
 
-    //open the fifo
-    int fd1,fd2;
-    fd1=open(writeFifo,O_WRONLY);
-    if(fd1==-1){
-        perror("open file");
-        //fprintf(stderr,"Error opening fifo1 in P1\n");
-        exit(1);
-    }
-    fd2=open(readFifo,O_RDONLY);
-    if(fd2==-1){
-        perror("open file");
-        //fprintf(stderr,"Error opening fifo2 in P1");
-        exit(1);
-    }
+    msgid1=msgget(keyq1,0666 | IPC_CREAT); //create or open the msg queue
+    msgid2=msgget(keyq2, 0666 | IPC_CREAT);
+    
+    msg1.mtype=1;
+    msg2.mtype=1;
 
     while(1){
 
         int n=rand_r(&seed)%SHM_SIZE;
+        //write the size in the message struct
+        msg1.size=n;
         //allocate string
         string=malloc(n*sizeof(char));
 
@@ -110,16 +114,17 @@ void P1(char *readFifo,char *writeFifo,int shmid){
         }
         //if n==0 i write 0 and the child knows that he has to terminate
         if(n==0){
-            if(write(fd1,&n,sizeof(int))!=sizeof(int)){
-                fprintf(stderr,"Erron in write\n");
-                exit(1);
-            }
+            //send the message
+            msgsnd(msgid1,&msg1,sizeof(message),0);
             fprintf(stdout,"P1 generates 0 : stopped\n");
             //detach memory
             if(shmdt(data)==-1){
                 perror("shmdt");
                 exit(1);
             }
+            //remove the msg queue
+            msgctl(msgid1,IPC_RMID,NULL);
+            msgctl(msgid2,IPC_RMID,NULL);
             return;
         }
 
@@ -142,31 +147,27 @@ void P1(char *readFifo,char *writeFifo,int shmid){
         //write the string
         strncpy(data,string,SHM_SIZE);
 
-
-        //wake up P2 writing n in the pipe
-        if(write(fd1,&n,sizeof(int))!=sizeof(int)){
-            fprintf(stderr,"Erron in write\n");
-            exit(1);
-        }
+        //wake up P2 writing n in the msg
+        msgsnd(msgid1,&msg1,sizeof(message),0);
 
         //wait that P2 writes
-        if(read(fd2,&n,sizeof(int))<0){
-            fprintf(stderr,"Error in read P1\n");
-            exit(1);
-        }
+        msgrcv(msgid2,&msg2,sizeof(message),1,0);
 
-        if(n==0){
+
+        if(msg2.size==0){
             fprintf(stdout,"P1 receives 0 : STOPPED\n\n");
             if(shmdt(data)==-1){
                 perror("shmdt");
                 exit(1);
-            }            
+            }    
+            msgctl(msgid1,IPC_RMID,NULL);
+            msgctl(msgid2,IPC_RMID,NULL);                    
             return;
         }
 
         fprintf(stdout,"P1 receives: %s\n\n",data);
         //toUpper
-        upper_string(data,n);
+        upper_string(data,msg2.size);
 
         fprintf(stdout,"---P1 convert: %s\n\n",data);
 
@@ -177,11 +178,18 @@ void P1(char *readFifo,char *writeFifo,int shmid){
         perror("shmdt");
         exit(1);
     }
+    msgctl(msgid1,IPC_RMID,NULL);
+    msgctl(msgid2,IPC_RMID,NULL);    
 }
-void P2(char *readFifo,char *writeFifo,int shmid){
+void P2(key_t keyq1,key_t keyq2,int shmid){
     int n,i;
     char *string,*data;
     unsigned int seed=1;
+    int msgid1,msgid2;
+    message msg1,msg2;
+
+    msgid1=msgget(keyq1,0666 | IPC_CREAT); //create or open the msg queue
+    msgid2=msgget(keyq2, 0666 | IPC_CREAT);
 
     //write on the shared memory
     data=shmat(shmid,NULL,0);
@@ -189,56 +197,51 @@ void P2(char *readFifo,char *writeFifo,int shmid){
         perror("shmat");
         exit(1);
     }
-    int fd1,fd2;
-    fd1=open(readFifo,O_RDONLY);
-    if(fd1==-1){
-        fprintf(stderr,"Error opening fifo1 in P2\n");
-        exit(1);
-    }
-    fd2=open(writeFifo,O_WRONLY);
-    if(fd2==-1){
-        fprintf(stderr,"Error opening fifo2 in P2\n");
-        exit(1);
-    }
+
+    msg1.mtype=1;
+    msg2.mtype=1;
 
     while(1){
         //P2
-        if(read(fd1,&n,sizeof(int))<0){
-            fprintf(stderr,"Error in read P2\n");
-            exit(1);
-        }
-        //check if n==0
-        if(n==0){
+        //receive the msg
+        msgrcv(msgid2,&msg2,sizeof(message),1,0);
+        //check if msg2.size==0
+        if(msg2.size==0){
             fprintf(stdout,"P2 receives 0 : STOPPED\n");
             if(shmdt(data)==-1){
                 perror("shmdt");
                 exit(1);
-            }            
+            }
+            msgctl(msgid1,IPC_RMID,NULL);
+            msgctl(msgid2,IPC_RMID,NULL);              
             return;
         }
 
 
         fprintf(stdout,"P2 receives: %s\n\n",data);
         //toUpper
-        upper_string(data,n);
+        upper_string(data,msg2.size);
 
         fprintf(stdout,"---P2 convert: %s\n\n",data);
 
         //generate string and write on shmem
         int n=rand_r(&seed)%SHM_SIZE;
 
+        //WRITE n in the msg
+        msg1.size=n;
+
 
         //if n==0 i write 0 and the child knows that he has to terminate
         if(n==0){
-            if(write(fd2,&n,sizeof(int))!=sizeof(int)){
-                fprintf(stderr,"Erron in write\n");
-                exit(1);
-            }
+            //send the message
+            msgsnd(msgid1,&msg1,sizeof(message),0);
             fprintf(stdout,"P2 receives 0 : STOPPED\n");
             if(shmdt(data)==-1){
                 perror("shmdt");
                 exit(1);
-            }           
+            }
+            msgctl(msgid1,IPC_RMID,NULL);
+            msgctl(msgid2,IPC_RMID,NULL);           
             return;
         }
 
@@ -263,10 +266,7 @@ void P2(char *readFifo,char *writeFifo,int shmid){
         strncpy(data,string,SHM_SIZE);
 
         //wake up P2 writing n in the pipe
-        if(write(fd2,&n,sizeof(int))!=sizeof(int)){
-            fprintf(stderr,"Erron in write\n");
-            exit(1);
-        }
+        msgsnd(msgid1,&msg2,sizeof(message),0);
         free(string);
 
 
@@ -275,6 +275,8 @@ void P2(char *readFifo,char *writeFifo,int shmid){
         perror("shmdt");
         exit(1);
     }
+    msgctl(msgid1,IPC_RMID,NULL);
+    msgctl(msgid2,IPC_RMID,NULL);   
 }
 
 void upper_string(char *str,int length){
